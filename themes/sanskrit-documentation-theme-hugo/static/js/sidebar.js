e6d76c35c0fe05a6f9c921cc5c47d337b0b501a5
vvasuki <vishvas.vasuki@gmail.com>
2018-12-29 16:33:06 -0800

    a

--- themes/sanskrit-documentation-theme-hugo/static/js/sidebar.js
+++ themes/sanskrit-documentation-theme-hugo/static/js/sidebar.js
@@ -98,3 +98,17 @@ $(document).ready(function() {
     //     }
     // });
 });
+
+// Code to make the "Nav" button, which toggles the sidebar.
+var toggleSidebar = function() {
+    $("#sidebar").toggle();
+    // $("#tg-sb-icon").toggleClass('fa-toggle-on');
+    // $("#tg-sb-icon").toggleClass('fa-toggle-off');
+    $("#tg-sb-icon-content-pane").toggleClass('fa-toggle-on');
+    $("#tg-sb-icon-content-pane").toggleClass('fa-toggle-off');
+};
+
+$(document).ready(function() {
+    $("#tg-sb-link").click(toggleSidebar);
+    // $("#hide-sb-link").click(toggleSidebar);
+});
